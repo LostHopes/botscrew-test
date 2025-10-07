39c7db1b7f3657187ad1aaf9221714d01d11f948
losthope <arsen.melnuchyk@gmail.com>
2025-10-07 10:09:40 +0300

    feat: add error handling

--- src/main/java/com/botscrew/university/UniversityApplication.java
+++ src/main/java/com/botscrew/university/UniversityApplication.java
@@ -1,6 +1,7 @@
 package com.botscrew.university;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.Scanner;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.botscrew.university.dto.Department;
 import com.botscrew.university.dto.Lector;
 import com.botscrew.university.dto.Role;
 import com.botscrew.university.misc.ChoiceMenu;
@@ -23,6 +25,7 @@ public class UniversityApplication implements CommandLineRunner {
 
 	private final SampleData sampleData;
 	private final DepartmentRepository departmentRepository;
+	Scanner scanner = new Scanner(System.in);
 
 	public UniversityApplication(SampleData sampleData, DepartmentRepository departmentRepository) {
 		this.sampleData = sampleData;
@@ -34,25 +37,39 @@ public class UniversityApplication implements CommandLineRunner {
 
 		sampleData.generate();
 
-		Scanner scanner = new Scanner(System.in);
+		List<Department> departments = departmentRepository.findAll();
+		for (Department department : departments) {
+			System.out.println(department.getName());
+		}
 
-		// TODO: add error error handlers for Scanner
-		System.out.print("Enter department name: ");
-		String departmentName = scanner.nextLine().strip();
+		System.out.print("Enter department name from available above:");
 
-		System.out.println(
+		try {
+			String departmentName = scanner.nextLine().strip();
+			System.out.println(
 			"""
 				1. Who is the head of department?
 				2. Show department statistics
 				3. Show the average salary for the department
 				4. Show count of employee for department
 				5. Global search\n"""
-		);
+			);
 
-		System.out.print("Select number:");
-		int userInput = scanner.nextInt();
+			System.out.print("Select number:");
+			int choiceFromMenu = scanner.nextInt();
 
-		switch(userInput) {
+			this.menu(departmentName, choiceFromMenu);
+		
+			scanner.close();
+		} catch (NoSuchElementException e) {
+			System.err.println(String.format(" %s", e.getMessage()));
+		}
+
+		
+	}
+
+	private void menu(String departmentName, int choice) {
+		switch(choice) {
 			case ChoiceMenu.HEAD_OF_DEPARTMENT:
 
 				Optional<Lector> head = departmentRepository.findDepartmentHead(departmentName);
@@ -62,7 +79,7 @@ public class UniversityApplication implements CommandLineRunner {
 					break;
 				}
 
-				System.out.println(String.format("Head of the department is %s", head.get().getName()));
+				System.out.println(String.format("Head of the %s department is %s", departmentName, head.get().getName()));
 				break;
 			
 			case ChoiceMenu.SHOW_DEPARTMENT_STATISTICS:
@@ -81,7 +98,7 @@ public class UniversityApplication implements CommandLineRunner {
 			
 			case ChoiceMenu.SHOW_AVERAGE_SALARY:
 				int salary = departmentRepository.calculateAvgSalary(departmentName);
-				System.out.println(String.format("Average salary for department: %d UAH", salary));
+				System.out.println(String.format("Average salary for %s department: %d UAH", departmentName, salary));
 				break;
 			
 			case ChoiceMenu.SHOW_COUNT_OF_EMPLOYEE:
@@ -90,17 +107,29 @@ public class UniversityApplication implements CommandLineRunner {
 				break;
 			
 			case ChoiceMenu.GLOBAL_SEARCH:
-				List<Lector> lectors = departmentRepository.searchLectorsByTemplate(departmentName, "ss");
+
+				scanner.nextLine();
+
+				System.out.print("Enter search template:");
+				String template = scanner.nextLine();
+
+				List<Lector> lectors = departmentRepository.searchLectorsByTemplate(departmentName, template);
+
+				if (lectors.size() == 0) {
+					System.out.println("Lectors with given search template doesn't exist");
+					break;
+				}
 
 				for (Lector lector : lectors) {
 					System.out.println(lector.getName());
 				}
+				System.out.println(String.format("\nFound %d lectors", lectors.size()));
 				break;
 			
 			default:
+				System.err.println("Invalid choice from the menu");
 				break;
 		}
-		scanner.close();
 	}
 
 }
